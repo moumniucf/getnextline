d23dd10f74c9c6048f26a89fae31c78f2a866116
moumniyoussef <youssef18moumni@gmail.com>
2024-12-03 15:56:00 +0100

    gg

--- ft_get_next_line.h
+++ ft_get_next_line.h
@@ -1,17 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_get_next_line.c                                 :+:      :+:    :+:   */
+/*   ft_get_next_line.h                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: youmoumn <youmoumn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/03 15:44:41 by youmoumn          #+#    #+#             */
-/*   Updated: 2024/12/03 15:46:02 by youmoumn         ###   ########.fr       */
+/*   Created: 2024/12/03 15:52:32 by youmoumn          #+#    #+#             */
+/*   Updated: 2024/12/03 15:55:48 by youmoumn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libc.h>
-char *get_next_line(int fd)
-{
-	
-}
+#ifndef FT_GET_NEXT_LINE_H
+# define FT_GET_NEXT_LINE_H
+
+# include <libc.h>
+
+char	*get_next_line(int fd);
+
+
+
+# endif
